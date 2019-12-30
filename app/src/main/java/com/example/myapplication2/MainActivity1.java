607121f54d62a7929834a02dfaed19477bc7a2b9
eagle86724 <eagle86724@gmail.com>
2019-12-30 22:44:24 +0800

    MyProject2 20191230

---
+++ app/src/main/java/com/example/myapplication2/MainActivity1.java
@@ -0,0 +1,16 @@
+package com.example.myapplication2;
+
+import java.lang.*;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication2/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.example.myapplication2;
+
+import java.lang.*;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
